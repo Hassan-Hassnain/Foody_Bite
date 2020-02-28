15f761c8cd8eea27b2c2463f98ddac37d7f5a162
Hassan Hassnain <ahh.devoders@gmail.com>
2020-02-29 00:41:59 +0500

    IBDezignable pod installed Launch screen and splash  screen Animation completed.

---
+++ Foody_Bite/New Group/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Foody_Bite
+//
+//  Created by Usama Sadiq on 2/28/20.
+//  Copyright © 2020 Usama Sadiq. All rights reserved.
+//
+
+import UIKit
+
+class StartVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 1.5) {
+            
+        }
+    }
+
+}
+
