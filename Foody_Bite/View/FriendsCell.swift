bd479cfe0ee1e7ad6c5ca1e71f7656dda17da28b
Hassan Hassnain <ahh.devoders@gmail.com>
2020-03-15 19:05:21 +0500

    ...

---
+++ Foody_Bite/View/FriendsCell.swift
@@ -0,0 +1,13 @@
+//
+//  FriendsCell.swift
+//  Foody_Bite
+//
+//  Created by Usama Sadiq on 3/2/20.
+//  Copyright © 2020 Usama Sadiq. All rights reserved.
+//
+
+import UIKit
+
+class VisitorsCell: UICollectionViewCell {
+    
+}
