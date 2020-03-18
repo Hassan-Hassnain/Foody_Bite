bfdbda569ffd7953cb8ebf921011d89dac3b2df4
Hassan Hassnain <ahh.devoders@gmail.com>
2020-03-18 23:40:41 +0500

    CreateUser and upload profile picture function added

--- Foody_Bite/Services/AuthService.swift
+++ Foody_Bite/Services/AuthService.swift
@@ -8,22 +8,11 @@
 
 import Foundation
 import Firebase
+import FirebaseStorage
 
 class AuthService {
     static let instance = AuthService()
-    
-    func registerUser(withName name: String, andEmail email: String, andPassword password: String, userCreationCompletion: @escaping (_ status: Bool, _ error: Error?) -> ()) {
-        Auth.auth().createUser(withEmail: email, password: password) { (AuthResult, error) in
-            
-            guard let user = AuthResult?.user, error == nil else { userCreationCompletion(false, error)
-                return
-            }
-            
-            let userData = ["name": user.displayName, "provider": user.providerID, "email": user.email]
-            DataService.instance.createDBUser(uid: user.uid, userData: userData as Dictionary<String, Any>)
-            userCreationCompletion(true, nil)
-        }
-    }
+
     
     func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error? ) -> ()) {
         Auth.auth().signIn(withEmail: email, password: password) { (AuthResult, error) in
--- Foody_Bite/Services/DataService.swift
+++ Foody_Bite/Services/DataService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Firebase
+import FirebaseStorage
 
 let DB_BASE = Database.database().reference()
 
@@ -18,6 +19,8 @@ class DataService {
     private var _REF_USERS = DB_BASE.child("users")
     private var _REF_GROUPS = DB_BASE.child("groups")
     private var _REF_FEED = DB_BASE.child("feed")
+    private var _REF_STORAGE = Storage.storage().reference(forURL: "gs://foody-bite.appspot.com")
+    
     
     var REF_BASE: DatabaseReference {
         return _REF_BASE
@@ -35,10 +38,22 @@ class DataService {
         return _REF_FEED
     }
     
+    var REF_STORAGE: StorageReference {
+        return _REF_STORAGE
+    }
+    
+    
     
     func createDBUser(uid: String, userData: Dictionary<String, Any>) {
-        REF_USERS.child(uid).updateChildValues(userData)
+        REF_USERS.child(uid).updateChildValues(userData, withCompletionBlock: {
+            (error, ref) in
+            if error == nil {
+                print("Done")
+            }
+        })
     }
+    
+
     
     private (set) var categores:[String] = ["Italian","Chinese","Maxican","Thai","Arabian","Indian","American","Korean","European"]
     
